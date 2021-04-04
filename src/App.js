61bf77c1f60487b49b83cde9b5d1a0b9d602aedc
Nathan Mackeans <nathanmackeansazah@gmail.com>
2021-04-04 16:49:52 +0100

    Tinder-frontend

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from './components/Header';
+import TinderCards from './components/TinderCards';
+import SwipeButtons from './components/SwipeButtons';
+function App() {
+  return (
+    <div className="App">
+    <Header />
+    <TinderCards />
+    <SwipeButtons />
+    </div>
+  );
+}
+
+export default App;
